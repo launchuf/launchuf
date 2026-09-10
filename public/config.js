c0b63f45a56428d6ae9b3add293a4f783f4a7784
launchuf <kontakt.launchuf@gmail.com>
2026-09-10 19:24:57 +0200

    Update config.js

--- public/config.js
+++ public/config.js
@@ -2,5 +2,5 @@ window.LAUNCH_CONFIG = {
   supabaseUrl: 'https://dqzcshsjuqhtynayrquw.supabase.co',
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRxemNzaHNqdXFodHluYXlycXV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkwMDIwNjIsImV4cCI6MjEwNDU3ODA2Mn0.VprURcSuLiiEhIatLL2EKxkKNIuFZnxtEaGsMyq9H7k',
   stripePublishableKey: 'YOUR_STRIPE_PUBLISHABLE_KEY',
-  prices: { GROW: 299, SCALE: 499 }
+  prices: { GROW: 299, SCALE: 498 }
 };
