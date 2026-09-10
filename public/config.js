dc1d99f7f01b2cfc4935d884eca7a7cfad2ab170
launchuf <kontakt.launchuf@gmail.com>
2026-09-10 16:52:41 +0200

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,8 +1,6 @@
-```
 window.LAUNCH_CONFIG = {
   supabaseUrl: 'https://dqzcshsjuqhtynayrquw.supabase.co',
   supabaseAnonKey: 'sb_publishable_eNAM2xt8VXJAN7iCcCNQ1Q_f1lZrOOm',
   stripePublishableKey: 'YOUR_STRIPE_PUBLISHABLE_KEY',
   prices: { GROW: 299, SCALE: 499 }
 };
-```
